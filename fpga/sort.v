6a890072f5821a4a45ebef4ed6fc228fd07b6ac6
kuchynski <kuchynskiandrei@gmail.com>
2023-04-21 17:38:17 +0200

    added automatic readiness detection

--- fpga/sort.v
+++ fpga/sort.v
@@ -14,13 +14,9 @@ module sort #(
     output reg active_output
 );
 
-    function integer clogb2 (input integer bit_depth);
-        for(clogb2 = 0; bit_depth > 0; clogb2 = clogb2 + 1)
-            bit_depth = bit_depth >> 1;
-    endfunction
-
     reg[WIDTH-1:0] registers[SIZE-1:0];
-    reg[clogb2(SIZE)+1:0] st = 0;
+    reg[SIZE-1:1] active;
+    reg[31:0] st = 0;
     integer i;
 
     always @(posedge clk) begin
@@ -29,6 +25,7 @@ module sort #(
             q <= 0;
             active_input <= 0;
             active_output <= 0;
+            active <= 1;
             for(i = 0; i < SIZE; i = i + 1)
                 registers[i] <= 0;
         end else begin
@@ -36,7 +33,7 @@ module sort #(
             active_input <= st < SIZE*2;
 
             if (st < 2) begin
-            end else if (st < SIZE*2 + 2) begin
+            end else if (st < SIZE*2 + 2) begin                
                 if (st[0] == 0) begin
                     registers[0] <= (d > registers[0])? d : registers[0];
                     for(i = 2; i < SIZE; i = i + 2) begin
@@ -49,20 +46,24 @@ module sort #(
                         registers[i]   <= (registers[i] > registers[i-1])? registers[i] : registers[i-1];
                     end
                 end
-            end else if (st < SIZE*4 + 2) begin
+            end else if (st < SIZE*3 + 2) begin
                 active_input <= 0;
                 if (st[0] == 0) begin
                     for(i = 2; i < SIZE; i = i + 2) begin
                         registers[i-1] <= (registers[i] > registers[i-1])? registers[i-1] : registers[i];
                         registers[i]   <= (registers[i] > registers[i-1])? registers[i] : registers[i-1];
+                        active[i] <= registers[i] < registers[i-1];
                     end
+                    if (active == 0)
+                        st <= SIZE*3 + 2;
                 end else begin
                     for(i = 1; i < SIZE; i = i + 2) begin
                         registers[i-1] <= (registers[i] > registers[i-1])? registers[i-1] : registers[i];
                         registers[i]   <= (registers[i] > registers[i-1])? registers[i] : registers[i-1];
+                        active[i] <= registers[i] < registers[i-1];
                     end
                 end
-            end else if (st < SIZE*5 + 2) begin
+            end else if (st < SIZE*4 + 2) begin
                 active_output <= 1;
                 for(i = 1; i < SIZE; i = i + 1)
                     registers[i] <= registers[i-1];
